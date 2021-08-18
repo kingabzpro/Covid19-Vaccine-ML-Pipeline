92c187cec5de36d551d3f66be4df61e482667ec9
Abid Ali Awan <abidaliawan@rocketmail.com>
2021-08-18 17:14:38 +0000

    master-merge

---
+++ load-data.py
@@ -0,0 +1,16 @@
+import orchest
+import pandas as pd
+
+
+# Convert the data into a DataFrame.
+INPUT_DIR = "Data"
+bcell = pd.read_csv(f"{INPUT_DIR}/input_bcell.csv")
+covid = pd.read_csv(f"{INPUT_DIR}/input_covid.csv")
+sars = pd.read_csv(f"{INPUT_DIR}/input_sars.csv")
+bcell_sars = pd.concat([bcell, sars], axis=0, ignore_index=True)
+
+# Output the housing data so the next steps can retrieve it.
+print("Outputting converted Vaccine data...")
+orchest.output((bcell, covid, sars, bcell_sars), name="data")
+print(bcell_sars.shape)
+print("Success!")
