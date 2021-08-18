6f779b84321438ff81e852bae326a35370de45ba
Abid Ali Awan <abidaliawan@rocketmail.com>
2021-08-18 15:37:40 +0000

    load data and orchest file

---
+++ .ipynb_checkpoints/load-data-checkpoint.py
@@ -0,0 +1,16 @@
+import orchest
+import pandas as pd
+
+
+# Convert the data into a DataFrame.
+INPUT_DIR = "Data"
+bcell = pd.read_csv(f"{INPUT_DIR}/input_bcell.csv")
+covid = pd.read_csv(f"{INPUT_DIR}/input_covid.csv")
+sars = pd.read_csv(f"{INPUT_DIR}/input_sars.csv")
+bcell_sars = pd.concat([bcell, sars], axis=0, ignore_index=True)
+
+# Output the housing data so the next steps can retrieve it.
+print("Outputting converted Vaccine data...")
+orchest.output((bcell_sars, covid, ), name="data")
+print(bcell_sars.head())
+print("Success!")
